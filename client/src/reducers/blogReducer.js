c29f9a29eac1a72ca52d87156bad26d45cf1b426
blackbeltdiaries <github email address>
2019-06-05 14:43:51 -0600

    created blogReducer.js in /reducers and added state, [], LOADING_POSTS, and FETCH_BLOG_POSTS

---
+++ client/src/reducers/blogReducer.js
@@ -0,0 +1,20 @@
+export default function blogReducer(
+  state ={
+    loading: false,
+    posts: []
+  }, action) {
+    switch (action.type) {
+
+      case 'LOADING_POSTS':
+        console.log('blog reducer loading posts')
+        return {...state, loading: true};
+
+      case 'FETCH_BLOG_POSTS':
+        console.log('blog reducer fetch posts')
+        return {loading: false, posts: action.payload}
+
+      default:
+        return state
+    }
+  }
+)
