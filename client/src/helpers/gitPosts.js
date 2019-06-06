ffe0824e9821259acdc772b2d62acfdbce71139c
blackbeltdiaries <github email address>
2019-06-06 15:22:02 -0600

    added base64 npm install

--- client/src/helpers/gitPosts.js
+++ client/src/helpers/gitPosts.js
@@ -1,10 +1,10 @@
+import { BASE64 } from 'js-base64';
+
 export const handleGitPosts = (gitHash) => {
   if (gitHash.postsLoading === false && gitHash.gitPosts.length > 0) {
     let posts = gitHash.gitPosts
 
-    console.log(posts)
-  //  gitPosts.forEach((post) => {
-    //  console.log(post)
-  //  });
+    let contentArray = posts.map(post => Base64.decode(post.content))
+    console.log(contentArray)
   }
 }
