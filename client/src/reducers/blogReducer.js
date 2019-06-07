97d732d438becf8c461d8394a1769c66b1770531
blackbeltdiaries <github email address>
2019-06-07 09:52:00 -0600

    added cases to switch function in gitReducer

--- client/src/reducers/blogReducer.js
+++ client/src/reducers/blogReducer.js
@@ -1,30 +1,15 @@
 export default function blogReducer(
   state ={
     loading: false,
-    postLoading: false,
-    postsLoading: false,
     posts: [],
-    gitPosts: [],
   }, action) {
     switch (action.type) {
 
       case 'LOADING_POSTS':
         return {...state, loading: true};
 
-      case 'LOADING_GIT_POSTS':
-        return {...state, postsLoading: true};
-
-      case 'LOADING_GIT_POST':
-        return {...state, postLoading: true};
-
       case 'FETCH_BLOG_POSTS':
         return {loading: false, posts: state.posts}
-
-      case 'FETCH_GITHUB_POST':
-        return {postLoading: false, gitPosts: [...state.gitPosts, action.payload]}
-
-      case 'FETCH_GITHUB_POSTS':
-        return {postsLoading: false, gitPosts: state.gitPosts};
 
       default:
         return state;
--- client/src/reducers/gitReducer.js
+++ client/src/reducers/gitReducer.js
@@ -1,6 +1,7 @@
 export default function gitReducer(
   state={
     loading: false,
+    postLoading: false,
     gitPosts: [],
   }, action) {
     switch (action.type) {
@@ -8,8 +9,14 @@ export default function gitReducer(
       case 'LOADING_POSTS':
         return {...state, loading: true};
 
+      case 'LOADING_POST':
+        return {...state, postLoading: true};
+
       case 'FETCH_GITHUB_POSTS':
         return {loading: false, gitPosts: state.gitPosts}
+
+      case 'FETCH_GITHUB_POST':
+        return {postLoading: false, gitPosts: [...state.gitPosts, action.payload]}
 
       default:
         return state;
