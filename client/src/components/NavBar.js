b3571ee33b388c65111760bdda06a5877bb5d5fe
blackbeltdiaries <github email address>
2019-06-08 16:39:50 -0600

    created and added to NavBar.css

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 // allows use of logo for menu button
 import logo from './monks_and_ninjas_logo.svg'
+// styling
+import './NavBar.css';
 // handles data used for NavBar in all pages
 class NavBar extends React.Component {
   render() {
     return (
       <div className="dropdown">
-        <NavLink to="/" exact activeStyle={{ background: 'grey' }}>Home</NavLink>
-        <NavLink to="/blog" exact activeStyle={{ background: 'grey' }}>Blog</NavLink>
+
+        <img src={logo} className="dropbtn" style={{ width: '5%', height: '5%' }} alt={logo} />
+          <div className="dropdown-content">
+            <NavLink to="/" exact activeStyle={{ background: 'grey' }}>Home</NavLink>
+            <NavLink to="/blog" exact activeStyle={{ background: 'grey' }}>Blog</NavLink>
+          </div>
       </div>
     );
   }
