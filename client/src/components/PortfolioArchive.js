59d38359a41ac49e0d0f77e91bfa3938fad7e5c3
blackbeltdiaries <github email address>
2019-06-10 13:15:43 -0600

    created PortfolioArchive.js in /components and added class to it

---
+++ client/src/components/PortfolioArchive.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PortfolioArchive extends React.Component {
+  render() {
+    return (
+      <div className="portfolioWraper">
+        Portfolio Archive goes here!
+      </div>
+    )
+  }
+}
+
+export default PortfolioArchive;
--- client/src/containers/PortfolioContainer.js
+++ client/src/containers/PortfolioContainer.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import PortfolioArchive from '../components/PortfolioArchive';
 // handles portfolio data
 class PortfolioContainer extends React.Component {
   render() {
     return (
       <div className="container">
         Portfolio info goes here!!
+        <PortfolioArchive />
       </div>
     )
   }
