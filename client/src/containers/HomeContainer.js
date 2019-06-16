b040b12b01d676767eb6f1bb81297db66cf0c2e1
blackbeltdiaries <github email address>
2019-06-16 13:51:58 -0600

    added anchors in HomeContainer.js

--- client/src/containers/HomeContainer.js
+++ client/src/containers/HomeContainer.js
@@ -1,14 +1,32 @@
 import React from 'react';
 // renders Home page
 class HomeContainer extends React.Component {
+
+  handleClick = event => {
+    event.preventDefault()
+    let top = document.getElementById(event.target.value)
+
+    top.scrollIntoView();
+  }
+
   render() {
     return (
       <div className="container">
         <h1>Home Page!</h1>
-        <p><a href="#postAnchor">Blog</a></p>
-        <p><a href="#projectAnchor">Portfolio</a></p>
-        <p><a href="#artAnchor">Art</a></p>
-        <p><a href="#aboutAnchor">About Me</a></p>
+        <button value="postAnchor" onClick={event => this.handleClick(event)}>Blog</button>
+        <button value="projectAnchor" onClick={event => this.handleClick(event)}>Portfolio</button>
+        <button value="artAnchor" onClick={event => this.handleClick(event)}>Art</button>
+        <button value="aboutAnchor" onClick={event => this.handleClick(event)}>About Me</button>
+
+
+        <p style={{ height: '1500px' }}></p>
+        <div id="postAnchor">Posts News</div>
+        <p style={{ height: '1500px' }}></p>
+        <div id="projectAnchor">Projects News</div>
+        <p style={{ height: '1500px' }}></p>
+        <div id="artAnchor">Art News</div>
+        <p style={{ height: '1500px' }}></p>
+        <div id="aboutAnchor">About Me</div>
       </div>
     )
   }
