7c6f49dced3290137af7674a9658cb3cb3e25d57
blackbeltdiaries <github email address>
2019-06-18 14:46:42 -0600

    created and added class PDFJs to pdfjs.js

--- client/src/components/Resume/ResumeViewer.js
+++ client/src/components/Resume/ResumeViewer.js
@@ -3,7 +3,9 @@ import React from 'react';
 class ResumeViewer extends React.Component {
   render() {
     return (
-
+      <div id='viewer' style={{ width: '100%', height: '100%' }}>
+        Hello World!
+      </div>
     )
   }
 }
