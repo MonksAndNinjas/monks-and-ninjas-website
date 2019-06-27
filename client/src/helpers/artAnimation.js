b9a11bd975a6b051b2b028d59889f16dabc9e63b
blackbeltdiaries <github email address>
2019-06-27 11:18:20 -0600

    added notes and console.log to artAnimation.js

--- client/src/helpers/artAnimation.js
+++ client/src/helpers/artAnimation.js
@@ -4,6 +4,10 @@ export const onClickAnimation = () => {
   var removeImg3 = document.getElementById('highlight-art-3');
   var artContent = document.getElementById('viewArtWrapper')
 
+  console.log(removeImg1.getBoundingClientRect())
+  console.log(removeImg2.getBoundingClientRect())
+  console.log(removeImg3.getBoundingClientRect())
+
   removeImg1.style.display = 'none';
   removeImg2.style.display = 'none';
   removeImg3.style.display = 'none';
@@ -21,3 +25,14 @@ export const onCloseAnimation = () => {
   removeImg3.style.display = 'inline-table';
   artContent.style.display = 'none';
 }
+
+// #highlight1, #highlight2, #highlight3 => { position: relative; z-indez: (3, 2, 1); transform: translate(what is the value to move) }
+
+
+// first I need to pick point for them to meet - use highlight1 as reference
+// second get the 3 div positions and width
+// third create forumla to make the left side edge touch for each div
+// fourth after meeting edges rotate the unselected divs
+// then able to reverse
+
+// increases height for selected and adjusts height for unselected, making them the same width and height
