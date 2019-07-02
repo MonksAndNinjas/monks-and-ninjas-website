90129991dd687a2cd012720bc09a7c1abd82a062
blackbeltdiaries <github email address>
2019-07-02 14:35:54 -0600

    added migrations and seeds in Rails backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,85 +10,91 @@
 date = Time.now.strftime("%A, %d %Y - %T - %Z")
 post = Post.create(title: 'test', content: 'this is a test post', datePublished: date, lastPublished: date)
 
-
-# new format for projects
-# Project has many notes, and notes belongs to a project
-# projectN = Project.create(
-#  title:
-#  description:
-#  notes: [note1, note2, note3, note4]
-#  date:
-#  github:
-#  video:
-#  embed:
-#)
-
-# projects for portfolios
-#note1Project1 = Note.create(summary:)
-#note2Project1 = Note.create(summary:)
-#note3Project1 = Note.create(summary:)
+###############################################################################
 
 project1 = Project.create(
-             title: "CLI Data Gem",
-             description: "This Ruby Gem provides a CLI to view the world's most popular martial arts taken from a www.singpatong-sitnumnoi.com; Users can also learn more information about the style such as (wiki-website, description, name, country, fighting-focus). styles, can be searched by country, or fighting-focus. I have also added code to collect incomplete data from the scrape if one has the desire to add code to retrieve it.",
-             date: "June 7, 2019",
-             github: "https://github.com/MonksAndNinjas/Monks-and-Ninjas-React-App",
-             video: "https://www.youtube.com/watch?v=l0_jHQvCLZg&t=29s",
-             embed: "l0_jHQvCLZg"
-             )
+  title: "CLI Data Gem",
+  description: "This Ruby Gem provides a CLI to view the world’s most popular martial arts, providing information on country, fighting-focus, and style",
+  date: "June 7, 2019",
+  github: "https://github.com/MonksAndNinjas/Monks-and-Ninjas-React-App",
+  video: "https://www.youtube.com/watch?v=l0_jHQvCLZg&t=29s",
+  embed: "l0_jHQvCLZg"
+)
 
-#note1Project2 = Note.create()
-#note2Project2 = Note.create()
-#note3Project2 = Note.create()
-#note4Project2 = Note.create()
+note1Project1 = project1.notes.build(summary: "Developed a Ruby CLI GEM for interfacing with the application")
+note2Project1 = project1.notes.build(summary: "Utilized Nokogiri to scrape data from websites")
+note3Project1 = project1.notes.build(summary: "Implemented Ruby variable scopes, object instantiation, and iterated collections to manipulate data")
+
+project1.save
+
+################################################################################3
 
 project2 = Project.create(
              title: "Sinatra App",
-             description: "Welcome to Monks And Ninjas fitness site. Guests will see upcoming events, about us, teachers, and contact. If guest decides to signup, they will have access to our users section which includes connecting to our network of users, newsfeed, and posts. User will be able to create, destroy, read, and edit posts, as well as edit their account info. Sinatra and ActiveRecord are used to create the dynamic site.",
+             description: "Fitness site where guests will see upcoming events, about us, teachers, and contact. If guest decides to signup, they will have access to our users section which includes connecting to our network of users, newsfeed, and posts. User will be able to create, destroy, read, and edit posts, as well as edit their account info.",
              date: "August 16, 2018",
              github: "https://github.com/MonksAndNinjas/monks-and-ninjas-sinatra",
              video: "https://www.youtube.com/watch?v=pw0EEr80du8",
              embed: "pw0EEr80du8"
              )
 
-#note1Project3 = Note.create(summary:)
-#note2Project3 = Note.create(summary:)
-#note3Project3 = Note.create(summary:)
-#note4Project3 = Note.create(summary:)
+note1Project2 = project2.notes.build(summary: "Developed forms to collect user data and create account")
+note2Project2 = project2.notes.build(summary: "Incorporated routes to display and collect data")
+note3Project2 = project2.notes.build(summary: "Utilized basic login to manage user data")
+
+project2.save
+
+#################################################################################
 
 project3 = Project.create(
              title: "Rails App",
-             description: "The Monks and Ninjas Life Manager App allows users to efficiently structure and promote meaning in ones life. Once the app is setup the user will have access to recording family members, online subscriptions, short-term and long-term goals, weekly priorities, inspirations, and controlling bad habits.",
+             description: "Life Manager App allows users to manage online subscriptions, relationships, goals, to-do list, inspirations and control bad habits.",
              date: "February 5, 2019",
              github: "https://github.com/MonksAndNinjas/rails-monks-and-ninjas",
              video: "https://www.youtube.com/watch?v=zQ3gvS1YwPo&t=291s",
              embed: "zQ3gvS1YwPo"
              )
 
-#note1Project4 = Note.create(summary:)
-#note2Project4 = Note.create(summary:)
-#note3Project4 = Note.create(summary:)
-#note4Project4 = Note.create(summary:)
+note1Project3 = project3.notes.build(summary: "Utilized OmniAuth and Facebook to manage user authentication")
+note2Project3 = project3.notes.build(summary: "Incorporated Active Record Associations and nested routes")
+note3Project3 = project3.notes.build(summary: "Implemented Rails Active Record to persist and manage user data")
+
+project3.save
+
+#######################################################################################
 
 project4 = Project.create(
              title: "Rails JavaScript App",
-             description: "The Monks and Ninjas Life Manager App allows users to efficiently structure and promote meaning in ones life. Once the app is setup the user will have access to recording family members, online subscriptions, short-term and long-term goals, weekly priorities, inspirations, and controlling bad habits. The page 'my life', which are accessible through the icon on the top right of the screen is similar to all the other attribute pages except that it uses the Ruby code of those pages and implements it through Javascript. All the attributes and their features are accessible through just one page. The Javascript code makes use of Rails active model serializer and uses Javascript object models to make use of the JSON response. The page 'my quests' is also accessible through the icon on the top right of the screen. This page serves as a show page for the quests attribute and it display a bit more information than normal using Javascript                 to handle the data.",
+             description: "Life Manager App allows users to manage online subscriptions, relationships, goals, to-do list, inspirations and control bad habits.",
              date: "March 18, 2019",
              github: "https://github.com/MonksAndNinjas/rails-monks-and-ninjas/tree/monks-and-ninjas-js",
              video: "https://www.youtube.com/watch?v=3veaiRz-DBA&t=171s",
              embed: "3veaiRz-DBA"
              )
 
-#note1Project5 = Note.create(summary:)
-#note2Project5 = Note.create(summary:)
-#note3Project5 = Note.create(summary:)
-#note4Project5 = Note.create(summary:)
+note1Project4 = project4.notes.build(summary: "Utilized Active Record Serializer to pass data from Rails backend in JSON format")
+note2Project4 = project4.notes.build(summary: "Incorporated JavaScript to render data dynamically and handle JSON responses")
+note3Project4 = project4.notes.build(summary: "Utlized JavaScript to create one page dynamic routes")
+note4Project4 = project4.notes.build(summary: "Developed a Rails backend consisting of nested routes for the various attributes")
+
+project4.save
+
+##################################################################################
 
 project5 = Project.create(
              title: "React",
-             description: "The Monks and Ninjas Fitness Blog/Appointment App allows visitors to make a reservation for a fitness appointment as well as read blog posts. Administrators can manage appointments and submit new blog posts or delete old ones.",
+             description: "Fitness Blog/Appointment App allows visitors to make reservations for a fitness appointment as well as read blog posts. Administrators canmanage appointments and submit new blog posts or delete old ones.",
              date: "May 30, 2019",
              github: "https://github.com/MonksAndNinjas/Monks-and-Ninjas-React-App",
              video: "https://www.youtube.com/watch?v=P-v1Tc50bFs",
              embed: "P-v1Tc50bFs"
              )
+
+note1Project5 = project5.notes.build(summary: "Developed a Rails API backend with endpoints for appointments and blog posts")
+note2Project5 = project5.notes.build(summary: "Developed ReactJS frontend to render data from API")
+note3Project5 = project5.notes.build(summary: "Implemented user interface employing React and Redux to connect to store")
+note4Project5 = project5.notes.build(summary: "Utilized JS fetch calls and promises to retrieve and handle JSON data from API ")
+
+project5.save
+
+####################################################################################
