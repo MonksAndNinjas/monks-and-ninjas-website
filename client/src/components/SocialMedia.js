4d5058a1ad7cab6002a30d8fcf13edc705e01117
blackbeltdiaries <github email address>
2019-07-30 10:36:53 -0600

    added SocialIcon componentsto SocialMedia.js

---
+++ client/src/components/SocialMedia.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// helper for social icons
+import { SocialIcon } from 'react-social-icons';
+// styling
+// displays Social Media icons in footer
+class SocialMedia extends React.Component {
+
+  render() {
+    return (
+      <div id="socialMedia">
+        <SocialIcon style={{ overflow: 'visible', margin: '10px', height: '25px', width: '25px' }} url="https://www.instagram.com/monksandninjas/?hl=en" />
+        <SocialIcon style={{ overflow: 'visible', margin: '10px', height: '25px', width: '25px' }} url="https://www.linkedin.com/in/joseph-jimenez-31794711b/" />
+      </div>
+    );
+  }
+}
+
+export default SocialMedia;
