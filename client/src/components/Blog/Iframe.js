7304bbd36f82b974bcdc77bc7b857864f256a3a7
blackbeltdiaries <github email address>
2019-09-06 11:14:30 -0600

    added constants and links in Iframe.js

--- client/src/components/Blog/Iframe.js
+++ client/src/components/Blog/Iframe.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// pulls codepen iframe data in string form and turns it into iframe tag
 class Iframe extends React.Component {
 
   render() {
@@ -8,13 +8,18 @@ class Iframe extends React.Component {
     const iframeData = string.split('<iframe')[1].split('>')[0];
     const title = iframeData.split('title="')[1].split('" src')[0];
     const source = iframeData.split('src="')[1].split('"')[0];
+    const visitPen = string.split('result">')[1].split("href='")[1].split("'>");
 
-    console.log(string);
+    //console.log(string);
 
     return(
       <div className="codePenWrapper">
-        <iframe title={title} src={source} height="265" width="100%" scrolling="no" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
-
+        <iframe title={title} src={source} height="400" width="100%" scrolling="no" frameBorder="no" allowtransparency="true" allowFullScreen={true}>
+          See the Pen (
+            <a href={visitPen}>{title}</a>
+            by MonksAndNinjas
+            <a href="https://codepen.io/monksandninjas">@monksandninjas</a>
+          ) on <a href="https://codepen.io">CodePen</a>.
         </iframe>
       </div>
     )
